590b7de0338b99d333052081b210959386e3b1c6
GwynBlei <144176510+GwynBlei@users.noreply.github.com>
2023-09-08 21:12:07 +0300

    Update Main.kt

--- Practice2/Practice2/src/main/kotlin/Main.kt
+++ Practice2/Practice2/src/main/kotlin/Main.kt
@@ -1,18 +1,18 @@
 fun main(args: Array<String>) {
     // Задача 1
-       val age1: Int = 32;
+       val age1: Int = 31;
        val age2: Int = 21;
        println(age1);
        println(age2);
 
    // Задача 2
-      val avg1: Int = (32 + 21) / 2;
+      val avg1: Int = (31 + 21) / 2;
       println(avg1);
     // ответ выводится без остатка
 
 
    // Задача 3
-      val avg2: Double = (32.0 + 21.0) / 2;
+      val avg2: Double = (31.0 + 21.0) / 2;
       println(avg2);
 
 
